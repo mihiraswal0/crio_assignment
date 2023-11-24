332d7dc4205b6aade0774ed8fea1bc7931514e51
Mihir Aswal <montyaswal0@gmail.com>
2023-11-24 20:27:37 +0530

    activity 2

---
+++ src/components1/Parent.jsx
@@ -0,0 +1,25 @@
+import React, { useState,useCallback } from 'react'
+import Child from './Child';
+import Title from './Title';
+const Parent = () => {
+    const [parentCnt, setParentCnt] = useState(0);
+    const [childCnt, setChildCnt] = useState(0);
+    const increaseParent = () => {
+        setParentCnt(prev => prev + 1);
+    }
+    const increaseChild = useCallback(() => {
+        setChildCnt(prev => prev + 1);
+    },[]);
+    console.log("Parent Component rendering")
+    return (
+        <div >
+            <Title/>
+            <label>Parent Counter: </label>
+            {parentCnt}
+            <button onClick={increaseParent}>Parent Button</button>
+            <Child childCnt={childCnt} increaseChild={increaseChild} />
+        </div>
+    )
+}
+
+export default React.memo(Parent)
